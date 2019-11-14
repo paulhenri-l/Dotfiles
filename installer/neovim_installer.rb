9c53b6e8360b1b69fb946fc6b8a933c872b30363
paulhenri-l <25308170+paulhenri-l@users.noreply.github.com>
2019-11-15 00:25:58 +0100

    New nvim installer

--- installer/neovim_installer.rb
+++ installer/neovim_installer.rb
@@ -1,29 +1,27 @@
 class NeovimInstaller
-  def initialize()
-  end
-
-  def install!
-    install_python_extension
-    install_ruby_extension
+  def run!
+    puts "Installing neovim"
+    install_prerequisites
     install_plug
     install_plugins
+    puts " - Done"
   end
 
   private
 
-  def install_python_extension
-    system 'pip3 install --upgrade neovim'
-  end
-
-  def install_ruby_extension
-    system 'gem install neovim'
+  def install_prerequisites
+    puts " - Install prerequisites"
+    system "pip3 install --upgrade neovim"
+    system "gem install neovim"
   end
 
   def install_plug
-    system 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    puts " - Install plug"
+    system "curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
   end
 
   def install_plugins
-    system 'nvim +PlugInstall +PlugClean +UpdateRemotePlugins +qall'
+    puts " - Install plugins"
+    system "nvim +PlugInstall +PlugClean +UpdateRemotePlugins +qall"
   end
 end
